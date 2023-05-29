c5f0c1d252623fae74cfd455ee5305f68db22526
jovi <joao.marquesvale@gmail.com>
2023-05-28 22:13:04 -0300

    feat(feature/companies): add resource from company service to main resource struct

--- internal/services/services.go
+++ internal/services/services.go
@@ -1,11 +1,13 @@
 package services
 
 import (
+	"cybersafe-backend-api/internal/services/companies"
 	"cybersafe-backend-api/internal/services/courses"
 	"cybersafe-backend-api/internal/services/users"
 )
 
 type Resources struct {
-	Users   users.UsersManager
-	Courses courses.CoursesManager
+	Users     users.UsersManager
+	Courses   courses.CoursesManager
+	Companies companies.CompaniesManager
 }
