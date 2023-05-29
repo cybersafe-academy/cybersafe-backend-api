e9252855944a66fdecaa9922374e3b7641eb6b40
jovi <joao.marquesvale@gmail.com>
2023-05-28 22:10:25 -0300

    feat(feature/companies): add setup company routes function to main setup routes function

--- internal/api/handlers/routes.go
+++ internal/api/handlers/routes.go
@@ -4,6 +4,7 @@ import (
 	_ "cybersafe-backend-api/docs"
 	"cybersafe-backend-api/internal/api/components"
 	"cybersafe-backend-api/internal/api/handlers/authentication"
+	"cybersafe-backend-api/internal/api/handlers/companies"
 	"cybersafe-backend-api/internal/api/handlers/courses"
 	"cybersafe-backend-api/internal/api/handlers/users"
 	"net/http"
@@ -17,6 +18,7 @@ func SetupRoutes(c *components.Components) http.Handler {
 	subRouter.Mount("/auth", authentication.SetupRoutes(c))
 	subRouter.Mount("/users", users.SetupRoutes(c))
 	subRouter.Mount("/courses", courses.SetupRoutes(c))
+	subRouter.Mount("/companies", companies.SetupRoutes(c))
 
 	return subRouter
 }
