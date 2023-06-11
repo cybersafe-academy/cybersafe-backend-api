1a6941c6c23cb9e01981e6a88732874ca7ca6603
jovi <joao.marquesvale@gmail.com>
2023-06-11 20:26:47 -0300

    fix: update field on company model

--- internal/models/companies.go
+++ internal/models/companies.go
@@ -3,9 +3,9 @@ package models
 type Company struct {
 	Shared
 
-	BusinessName string `gorm:"unique"`
-	TradeName    string
-	CNPJ         string `gorm:"unique"`
-	Email        string `gorm:"unique"`
-	Phone        string
+	LegalName string `gorm:"unique"`
+	TradeName string
+	CNPJ      string `gorm:"unique"`
+	Email     string `gorm:"unique"`
+	Phone     string
 }
